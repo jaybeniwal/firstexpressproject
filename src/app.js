d3d4178ba9b2b2878c45db0131ef756182e8bf25
jay beniwal <jit123@vitj.ac.in>
2021-07-14 16:23:39 +0530

    secound commit

--- src/app.js
+++ src/app.js
@@ -1,16 +1,27 @@
 const express = require('express');
 const app = express();
-const port = 3000;
+const port =process.env.PORT || 3000;
 const hbs = require('hbs')
 const hostname = 'localhost';
 const path=require('path');
+
+// template path 
 const temppath= path.join(__dirname,"../template/views");
+// partials path 
 const partpath=path.join(__dirname,"../template/patials");
+// static path for public folder 
 const spath=path.join(__dirname,"../public");
+
+
 app.use(express.static(spath));
+
+// hbs
 app.set("view engine","hbs");
 app.set('views',temppath);
 hbs.registerPartials(partpath)
+
+// here is rendering 
+
 app.get("/",(req,res)=>{
     res.render('index.hbs');
 })
